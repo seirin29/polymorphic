6c0e10bba3770014f4dad906b0a70128d25885d2
seirin29 <31317917+seirin29@users.noreply.github.com>
2017-09-28 20:09:00 +0700

    [Laravel] Level 14 Relationship and Mass Assignment

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+	protected $fillable = ['name'];
+	
+	public function photos(){
+		return $this->morphMany('App\Photo', 'imageable');
+	}
 }
--- app/Staff.php
+++ app/Staff.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Staff extends Model
 {
     //
+	protected $fillable = ['name'];
+	
+	public function photos(){
+		return $this->morphMany('App\Photo', 'imageable');
+	}
 }
